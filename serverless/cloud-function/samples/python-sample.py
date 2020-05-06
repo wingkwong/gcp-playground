a8886f3a5c23bf523c3e767639f1a9f0210813af
wingkwong <wingkwong.code@gmail.com>
2020-05-06 16:11:19 +0800

    Add Sample Code

---
+++ serverless/cloud-function/samples/python-sample.py
@@ -0,0 +1,9 @@
+def hello_get(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+    """
+    return 'Hello World!'
